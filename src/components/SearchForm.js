1c2aee5a2c32bb6667de5a4c3f496acdb3039855
Ihar_Chumakou <ihar_chumakou@epam.com>
2025-03-18 15:34:55 +0300

    Updated SearchForm component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -2,7 +2,11 @@ import '../styles/SearchFormStyles.css';
 
 export default function SearchForm({initialSearch, onSearch}) {
     return (
-        <form className="searchForm" onSubmit={onSearch}>
+        <form className="searchForm"
+              onSubmit={(e) => {
+                  e.preventDefault();
+                  onSearch(e.target[0].value);
+              }}>
             <input className="searchForm__input" type="text" placeholder={initialSearch}/>
             <button type="submit" className="searchForm__button">Search</button>
         </form>

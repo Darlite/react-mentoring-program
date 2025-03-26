9638d889a1b21e0b0757ff31e37378b49ee0a2bc
Ihar_Chumakou <ihar_chumakou@epam.com>
2025-03-26 18:27:37 +0300

    Updated test with describe/it structure

--- src/components/__tests__/Counter.test.tsx
+++ src/components/__tests__/Counter.test.tsx
@@ -6,22 +6,24 @@ import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
 import userEvent from "@testing-library/user-event";
 
-test('checking initial value', () => {
-    const initialCount = 3;
-    render(<Counter initialCount={initialCount} />);
-    expect(screen.getByText(`Counter: ${initialCount}`)).toBeInTheDocument();
-});
+describe('Counter component', () => {
+    it('checking initial value', () => {
+        const initialCount = 3;
+        render(<Counter initialCount={initialCount} />);
+        expect(screen.getByText(`Counter: ${initialCount}`)).toBeInTheDocument();
+    });
 
-test('decrementing the value', () => {
-    const initialCount = 3;
-    render(<Counter initialCount={initialCount} />);
-    userEvent.click(screen.getByRole('button', { name: "-" }));
-    expect(screen.getByText(`Counter: ${initialCount - 1}`)).toBeInTheDocument();
-});
+    it('decrementing the value', () => {
+        const initialCount = 3;
+        render(<Counter initialCount={initialCount} />);
+        userEvent.click(screen.getByRole('button', { name: "-" }));
+        expect(screen.getByText(`Counter: ${initialCount - 1}`)).toBeInTheDocument();
+    });
 
-test('incrementing the value', () => {
-    const initialCount = 3;
-    render(<Counter initialCount={initialCount} />);
-    userEvent.click(screen.getByRole('button', { name: "+" }));
-    expect(screen.getByText(`Counter: ${initialCount + 1}`)).toBeInTheDocument();
-});
+    it('incrementing the value', () => {
+        const initialCount = 3;
+        render(<Counter initialCount={initialCount} />);
+        userEvent.click(screen.getByRole('button', { name: "+" }));
+        expect(screen.getByText(`Counter: ${initialCount + 1}`)).toBeInTheDocument();
+    });
+})
--- src/components/__tests__/SearchForm.test.tsx
+++ src/components/__tests__/SearchForm.test.tsx
@@ -3,31 +3,33 @@ import '@testing-library/jest-dom';
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
-test('has initial value', () => {
-    const initialSearchText = "What do you want to watch?";
-    render(<SearchForm initialSearch={initialSearchText}
-                       onSearch={() => {
-                       }}/>);
-    const placeholder = screen.getByPlaceholderText(initialSearchText);
-    expect(placeholder).toBeInTheDocument();
-});
+describe('SearchForm component', () => {
+    it('has initial value', () => {
+        const initialSearchText = "What do you want to watch?";
+        render(<SearchForm initialSearch={initialSearchText}
+                           onSearch={() => {
+                           }}/>);
+        const placeholder = screen.getByPlaceholderText(initialSearchText);
+        expect(placeholder).toBeInTheDocument();
+    });
 
-test('check onSubmit function call by button click', () => {
-    const onSearch = jest.fn();
-    const searchQuery = "Joker";
-    render(<SearchForm initialSearch={""}
-                       onSearch={onSearch}/>);
-    userEvent.type(screen.getByRole("textbox"), searchQuery);
-    userEvent.click(screen.getByRole("button"));
-    expect(onSearch).toHaveBeenCalledWith(searchQuery);
-});
+    it('check onSubmit function call by button click', () => {
+        const onSearch = jest.fn();
+        const searchQuery = "Joker";
+        render(<SearchForm initialSearch={""}
+                           onSearch={onSearch}/>);
+        userEvent.type(screen.getByRole("textbox"), searchQuery);
+        userEvent.click(screen.getByRole("button"));
+        expect(onSearch).toHaveBeenCalledWith(searchQuery);
+    });
 
-test('check onSubmit function call by Enter key', () => {
-    const onSearch = jest.fn();
-    const searchQuery = "Joker";
-    render(<SearchForm initialSearch={""}
-                       onSearch={onSearch}/>);
-    userEvent.type(screen.getByRole("textbox"), searchQuery);
-    userEvent.type(screen.getByRole("textbox"), `{enter}`);
-    expect(onSearch).toHaveBeenCalledWith(searchQuery);
-});
+    it('check onSubmit function call by Enter key', () => {
+        const onSearch = jest.fn();
+        const searchQuery = "Joker";
+        render(<SearchForm initialSearch={""}
+                           onSearch={onSearch}/>);
+        userEvent.type(screen.getByRole("textbox"), searchQuery);
+        userEvent.type(screen.getByRole("textbox"), `{enter}`);
+        expect(onSearch).toHaveBeenCalledWith(searchQuery);
+    });
+})

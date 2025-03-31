6d9957d0048af413d2bba6804fcf1d714863a11d
Ihar_Chumakou <ihar_chumakou@epam.com>
2025-03-31 16:50:18 +0300

    Added height: 100% on root elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import styles from './App.module.css';
 import Counter from "./components/Counter";
 import SearchForm from "./components/SearchForm";
 import GenreSelect from "./components/GenreSelect";
 import {genreNames} from "./constants";
 import {useState} from "react";
+import MovieTile from "./components/MovieTile";
 
 export default function App() {
     const [selectedGenre, setSelectedGenre] = useState<string>("Documentary");
@@ -17,13 +18,19 @@ export default function App() {
     }
 
     return (
-        <div className="App">
+        <div className={styles.app}>
             <Counter initialCount={3}/>
             <SearchForm initialSearch={"What do you want to watch?"}
                         onSearch={handleSearch}/>
             <GenreSelect genreNames={genreNames}
                          selectedGenre={selectedGenre}
                          onSelect={handleGenreSelect}/>
+            <MovieTile imageUrl={"images/Pulp_Fiction.png"}
+                       movieName={"Pulp Fiction"}
+                       releaseYear={1994}
+                       relevantGenres={["Crime", "Thriller"]}
+                       onClick={() => {}}
+            />
         </div>
     );
 }

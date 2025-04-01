39bd88aafbd95ba9b8d3d49524cee4da8d86db66
Ihar_Chumakou <ihar_chumakou@epam.com>
2025-04-01 17:35:47 +0300

    Added context menu to MovieTile

--- src/components/MovieTile.tsx
+++ src/components/MovieTile.tsx
@@ -1,5 +1,5 @@
 import styles from '../styles/MovieTile.module.css';
-import React from "react";
+import React, {useState} from "react";
 import {MovieDetailsData} from "../types/MovieDetailsData";
 
 interface MovieTileProps {
@@ -9,9 +9,37 @@ interface MovieTileProps {
 
 const MovieTile: React.FC<MovieTileProps> = ({movieDetails, onClick}) => {
     const { imageUrl, movieName, releaseYear, relevantGenres } = movieDetails;
+
+    const [isHovered, setHovered] = useState(false);
+    const [showContextMenu, setShowContextMenu] = useState(false);
+
+    function showPopUp() {
+        setShowContextMenu(true);
+    }
+
+    function closePopUp() {
+        setShowContextMenu(false);
+    }
+
     return (
-        <div className={styles.movieTile}>
-            <img src={imageUrl} alt={movieName} onClick={() => onClick(movieName)} />
+        <div className={styles.movieTile} onMouseEnter={() => setHovered(true)}
+             onMouseLeave={() => setHovered(false)}>
+            <img
+                 src={imageUrl}
+                 alt={movieName}
+                 onClick={() => onClick(movieName)} />
+            {isHovered && (
+                <span className={styles.kebabMenu}
+                      onClick={showPopUp}
+                >︙</span>
+            )}
+            {showContextMenu && (
+                <div className={styles.contextMenu}>
+                    <div className={styles.contextMenuClose} onClick={closePopUp}>X</div>
+                    <div className={styles.contextMenuOption}>Edit</div>
+                    <div className={styles.contextMenuOption}>Delete</div>
+                </div>
+            )}
             <span className={styles.movieTitleAndYear}>
                 <h2 className={styles.title}>{movieName}</h2>
                 <span className={styles.year}>{releaseYear}</span>

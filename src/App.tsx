0aa88e7686bcf16b74e4649efb0e9451ec6685ea
Ihar_Chumakou <ihar_chumakou@epam.com>
2025-04-02 17:42:28 +0300

    Added two more movies

--- src/App.tsx
+++ src/App.tsx
@@ -31,15 +31,38 @@ export default function App() {
         setSelectedSortControl(option);
     }
 
-    const moviesData = {
-        imageUrl: "images/Pulp_Fiction.png",
-        movieName: "Pulp Fiction",
-        releaseYear: 1994,
-        relevantGenres: ["Crime", "Thriller"],
-        duration: "2h 34m",
-        rating: 8.9,
-        description: "Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny, bizarre and uncalled-for incidents.—Soumitra",
-    }
+    const moviesDataList = [
+        {
+            id: 1,
+            imageUrl: "images/Pulp_Fiction.png",
+            movieName: "Pulp Fiction",
+            releaseYear: 1994,
+            relevantGenres: ["Crime", "Thriller"],
+            duration: "2h 34m",
+            rating: 8.9,
+            description: "Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny, bizarre and uncalled-for incidents.—Soumitra",
+        },
+        {
+            id: 2,
+            imageUrl: "images/Bohemian_Rhapsody.png",
+            movieName: "Bohemian Rhapsody",
+            releaseYear: 2018,
+            relevantGenres: ["Musical", "Documentary"],
+            duration: "2h 15m",
+            rating: 8.5,
+            description: "With his impeccable vocal abilities, Freddie Mercury and his rock band, Queen, achieve superstardom. However, amidst his skyrocketing success, he grapples with his ego, sexuality and a fatal illness.",
+        },
+        {
+            id: 3,
+            imageUrl: "images/Kill_Bill_Volume_2.png",
+            movieName: "Kill Bill: Vol. 2",
+            releaseYear: 2004,
+            relevantGenres: ["Action", "Thriller"],
+            duration: "2h 17m",
+            rating: 8.9,
+            description: "A pregnant woman, codenamed the Bride, sets out on a journey to kill her ex-boss, Bill, and targets his brother, Budd, and Elle Driver, the only two survivors of the Deadly Vipers Assassination Squad.",
+        },
+    ]
 
     return (
         <div className={styles.app}>
@@ -52,9 +75,14 @@ export default function App() {
                          onSelect={handleGenreSelect}/>
             <SortControl currentSelection={selectedSortControl}
                          onSelect={handleSortControlChange}/>
-            <MovieTile movieDetails={moviesData}
-                       onClick={handleTileClick}
-            />
+            <div className={styles.movieList}>
+                {moviesDataList.map((movie) => (
+                    <MovieTile key={movie.id}
+                               movieDetails={movie}
+                               onClick={handleTileClick}
+                    />
+                ))}
+            </div>
         </div>
     );
 }

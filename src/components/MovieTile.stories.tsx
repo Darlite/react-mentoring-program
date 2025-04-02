d956a90d7c1e4100d398ae0f48ce97b522a132f9
Ihar_Chumakou <ihar_chumakou@epam.com>
2025-04-02 17:43:48 +0300

    Moved mock movie object into separate file

--- src/components/MovieTile.stories.tsx
+++ src/components/MovieTile.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from "@storybook/react";
 import MovieTile from "./MovieTile";
 import {fn} from "@storybook/test";
+import {moviesData} from "../mocks/mockMovieData";
 
 
 const meta = {
@@ -15,16 +16,6 @@ const meta = {
 export default meta;
 
 type Story = StoryObj<typeof meta>;
-
-const moviesData = {
-    imageUrl: "images/Pulp_Fiction.png",
-    movieName: "Pulp Fiction",
-    releaseYear: 1994,
-    relevantGenres: ["Crime", "Thriller"],
-    duration: "2h 34m",
-    rating: 8.9,
-    description: "Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny, bizarre and uncalled-for incidents.—Soumitra",
-}
 
 export const Default: Story = {
     args: {
--- src/components/__tests__/MovieTile.test.tsx
+++ src/components/__tests__/MovieTile.test.tsx
@@ -2,16 +2,7 @@ import MovieTile from "../MovieTile";
 import React from "react";
 import {render, screen} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-
-const moviesData = {
-    imageUrl: "images/Pulp_Fiction.png",
-    movieName: "Pulp Fiction",
-    releaseYear: 1994,
-    relevantGenres: ["Crime", "Thriller"],
-    duration: "2h 34m",
-    rating: 8.9,
-    description: "Jules Winnfield (Samuel L. Jackson) and Vincent Vega (John Travolta) are two hit men who are out to retrieve a suitcase stolen from their employer, mob boss Marsellus Wallace (Ving Rhames). Wallace has also asked Vincent to take his wife Mia (Uma Thurman) out a few days later when Wallace himself will be out of town. Butch Coolidge (Bruce Willis) is an aging boxer who is paid by Wallace to lose his fight. The lives of these seemingly unrelated people are woven together comprising of a series of funny, bizarre and uncalled-for incidents.—Soumitra",
-}
+import {moviesData} from "../../mocks/mockMovieData";
 
 describe("MovieTile", () => {
     it("renders correctly", () => {

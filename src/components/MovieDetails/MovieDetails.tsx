e79948e0f0e095cd1febdbb89f2b33a55f2e7016
Ihar_Chumakou <ihar_chumakou@epam.com>
2025-04-15 16:32:59 +0300

    Added formatRuntime function

--- src/components/MovieDetails/MovieDetails.tsx
+++ src/components/MovieDetails/MovieDetails.tsx
@@ -2,6 +2,7 @@ import styles from './MovieDetails.module.css';
 import React from "react";
 import {MovieDetailsData} from "../../types/MovieDetailsData";
 import placeholderImage from "../../assets/images/placeholderPoster.jpg";
+import formatRuntime from "../../utils/formatRuntime";
 
 interface MovieDetailsProps {
     movieDetails: MovieDetailsData;
@@ -30,7 +31,7 @@ const MovieDetails: React.FC<MovieDetailsProps> = ({movieDetails, handleBackToSe
                     ))}
                 </p>
                 <span className={styles.releaseDate}>{release_date}</span>
-                <span className={styles.runtime}>{runtime}</span>
+                <span className={styles.runtime}>{formatRuntime(runtime)}</span>
                 <p className={styles.overview}>{overview}</p>
                 <span className={styles.searchButton}
                       role="button"

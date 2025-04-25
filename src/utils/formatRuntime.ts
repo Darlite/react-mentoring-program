b63b9794fa672f4c252efa931a637795672dde8a
Ihar_Chumakou <ihar_chumakou@epam.com>
2025-04-25 19:25:27 +0300

    handling null runtime from Backend

--- src/utils/formatRuntime.ts
+++ src/utils/formatRuntime.ts
@@ -1,4 +1,6 @@
 export default function formatRuntime(runtime: string) {
+    if (!runtime) return null;
+
     const parsedRuntime = parseFloat(runtime);
     const hours = Math.floor(parsedRuntime / 60);
     const minutes = parsedRuntime % 60;
